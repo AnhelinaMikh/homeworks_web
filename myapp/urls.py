e0aeb2687c8ed3e6503e500a82a3a1670b4c1a2c
Anhelina <linaangelinamih@gmail.com>
2025-05-26 13:08:46 +0200

    Basic template usage: added rendering with context, logic operators, loops, and URL tag

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .views import home, about, contacts  # Убедись, что эти функции есть в views.py
+from .views import home, about, contacts, index, first  # добавили first
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', index, name='index'),       # Главная страница
     path('about/', about, name='about'),
     path('contacts/', contacts, name='contacts'),
+    path('first/', first, name='first'),  # <- Вот здесь добавлен маршрут с именем 'first'
 ]
+
 
 #http://127.0.0.1:8000/
 
 #http://127.0.0.1:8000/about/
 
 #http://127.0.0.1:8000/contacts/
+
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+class MyClass:
+    string = ''
 
-from django.http import HttpResponse
+    def __init__(self, s):
+        self.string = s
 
 def home(request):
     return HttpResponse("Это главная страница")
@@ -13,4 +16,24 @@ def about(request):
 def contacts(request):
     return HttpResponse("Это страница с контактами")
 
+def index(request):
+    my_num = 33
+    my_str = 'some string'
+    my_dict = {"some_key": "some_value"}
+    my_list = ['list_first_item', 'list_second_item', 'list_third_item']
+    my_set = {'set_first_item', 'set_second_item', 'set_third_item'}
+    my_tuple = ('tuple_first_item', 'tuple_second_item', 'tuple_third_item')
+    my_class = MyClass('class string')
+    return render(request, 'index.html', {
+        'my_num': my_num,
+        'my_str': my_str,
+        'my_dict': my_dict,
+        'my_list': my_list,
+        'my_set': my_set,
+        'my_tuple': my_tuple,
+        'my_class': my_class,
+        'display_num': True #or write False
+    })
 
+def first(request):
+    return HttpResponse("Это страница First")
