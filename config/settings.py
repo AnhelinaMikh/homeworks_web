64af19f863a0906d1baa930214566c212eae7c9b
Anhelina <linaangelinamih@gmail.com>
2025-05-27 12:24:06 +0200

    Fix template: move {% extends %} as first tag and load static after it

--- config/settings.py
+++ config/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
