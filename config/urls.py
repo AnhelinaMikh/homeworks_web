77fbfb98e15eb9d5be0dd01c075658cc921eb73f
Anhelina <linaangelinamih@gmail.com>
2025-06-04 11:54:32 +0200

    URLS from homework

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# config/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # Подключаем маршруты приложения
+    path('', include('myapp.urls')),  
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -4,30 +4,35 @@ from datetime import datetime, time
 from .forms import MyForm
 from django.shortcuts import redirect
 from .forms import AuthorForm
+from django.views import View
+from django.http import Http404
 
+import datetime
 
 def create_author(request):
     if request.method == 'POST':
         form = AuthorForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('success-page')  # замените на вашу страницу
+            return redirect('success-page')  
     else:
         form = AuthorForm()
     return render(request, 'author_form.html', {'form': form})
 
-# to lesson 23
-def form_view(request):
-    # name = request.GET.get("my_name")
-    # return render(request, "form.html", {"name": name}) 
-    if request.method == 'POST':
+
+
+class FormView(View):
+    def get(self, request):
+        form = MyForm()
+        return render(request, 'form.html', {'form': form})
+
+    def post(self, request):
         form = MyForm(request.POST)
         if form.is_valid():
             return render(request, 'form_was_valid.html')
-    else:
-        form = MyForm()
+        return render(request, 'form.html', {'form': form})
 
-    return render(request, 'form.html', {'form': form})
+
 
 class MyClass:
     string = ''
@@ -54,29 +59,58 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
-# def index(request):
-#     my_num = 33
-#     my_str = 'some string'
-#     my_dict = {"some_key": "some_value"}
-#     my_list = ['list_first_item', 'list_second_item', 'list_third_item']
-#     my_set = {'set_first_item', 'set_second_item', 'set_third_item'}
-#     my_tuple = ('tuple_first_item', 'tuple_second_item', 'tuple_third_item')
-#     my_class = MyClass('class string')
-#     return render(request, 'index.html', {
-#         'my_num': my_num,
-#         'my_str': my_str,
-#         'my_dict': my_dict,
-#         'my_list': my_list,
-#         'my_set': my_set,
-#         'my_tuple': my_tuple,
-#         'my_class': my_class,
-#         'display_num': True,
-#         'now': datetime.now()
-#     })
-# 1st version
-
-# def first(request):
-#     return HttpResponse("Это страница First")
 
 def first(request):
     return render(request, 'first.html')
+
+def my_feed(request):
+    return HttpResponse("Страница с лентой пользователя по его подпискам")
+
+def article_detail(request, article_id):
+    return HttpResponse(f"Статья с ID {article_id}")
+
+def article_comment(request, article_id):
+    return HttpResponse(f"Добавление комментария к статье с ID {article_id}")
+
+def article_update(request, article_id):
+    return HttpResponse(f"Редактирование статьи с ID {article_id}")
+
+def article_delete(request, article_id):
+    return HttpResponse(f"Удаление статьи с ID {article_id}")
+
+def create_article(request):
+    return HttpResponse("Создание новой статьи")
+
+def topic_list(request):
+    return HttpResponse("Список всех тем")
+
+def topic_articles(request, topic_id):
+    return HttpResponse(f"Статьи по теме ID {topic_id}")
+
+def topic_subscribe(request, topic_id):
+    return HttpResponse(f"Подписка на тему ID {topic_id}")
+
+def topic_unsubscribe(request, topic_id):
+    return HttpResponse(f"Отписка от темы ID {topic_id}")
+
+def profile_view(request):
+    return HttpResponse("Профиль пользователя и подписки")
+
+def register_view(request):
+    return HttpResponse("Регистрация нового пользователя")
+
+def login_view(request):
+    return HttpResponse("Вход пользователя")
+
+def logout_view(request):
+    return HttpResponse("Выход пользователя")
+
+def set_password(request):
+    return HttpResponse("Смена пароля")
+
+def articles_by_date(request, year, month):
+    try:
+        datetime.datetime(year, month, 1)
+    except ValueError:
+        raise Http404("Неправильная дата")
+    return HttpResponse(f"Статьи за {month:02}/{year}")
